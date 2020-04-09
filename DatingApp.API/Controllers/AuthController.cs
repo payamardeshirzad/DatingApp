d30a9d4afd03f52f693a69c645823112472915cc
Payam Ardeshirzad <payam@ardeshirzad.com>
2020-04-09 16:24:28 +1200

    Add Jwt Token Authorization

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,10 +17,12 @@ namespace DatingApp.API.Controllers
     public class AuthController: ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto )
@@ -32,5 +40,47 @@ namespace DatingApp.API.Controllers
             return StatusCode(201);
         }
         
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            if(userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            // Create Claims that our token wants to have
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName)
+            };
+
+            // create a security to ensure that the token is a valid token when it comes back, the server needs to sign the token by using a security key
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            // Creating Siging Credentials
+            var creds= new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Time to create a security token descriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            // Add Token Handler sof that we can add user token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Create Token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+
+        }
     }
 }
